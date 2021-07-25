2c2a92f285d3c0868c485b8b46372f35478a142f
nanshaqundao <nanshax@hotmail.co.uk>
2021-07-25 20:26:19 +0100

    use version 8.0.x mysql connector as my local mysql is in version 8, the old version 5 doesn't work

---
+++ java/book3/webframework02/src/main/java/org/smart4j/chapter2/demo/Demo.java
@@ -0,0 +1,7 @@
+package org.smart4j.chapter2.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
--- java/book3/webframework02/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ java/book3/webframework02/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,22 +1,75 @@
 package org.smart4j.chapter2.service;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.util.PropsUtil;
 
 /**
  * 提供客户数据服务
  */
 public class CustomerService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
 
+    static {
+        Properties config = PropsUtil.loadProps("config.properties");
+        DRIVER = config.getProperty("jdbc.driver");
+        URL = config.getProperty("jdbc.url");
+        USERNAME = config.getProperty("jdbc.username");
+        PASSWORD = config.getProperty("jdbc.password");
 
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver class, ", e);
+        }
+    }
 
     /**
      * 获取客户列表
      */
     public List<Customer> getCustomerList() {
-        String sql = "SELECT * FROM customer";
+        String sql = "SELECT * FROM demo.customer";
         //return DatabaseHelper.queryEntityList(Customer.class, sql);
+        Connection connection = null;
+        try {
+            List<Customer> customerList = new ArrayList<>();
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setId(resultSet.getLong("id"));
+                customer.setName(resultSet.getString("name"));
+                customer.setContact(resultSet.getString("contact"));
+                customer.setTelephone(resultSet.getString("telephone"));
+                customer.setEmail(resultSet.getString("email"));
+                customer.setRemark(resultSet.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException e) {
+            LOGGER.error("execute sql failure, ", e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOGGER.error("close connection failure ", e);
+                }
+            }
+        }
         return null;
     }
 

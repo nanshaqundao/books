e7b4f00b76708100da7161bc3dd3575e3e2c889d
nanshaqundao <nanshax@hotmail.co.uk>
2021-07-25 20:47:10 +0100

    create Database helper to extract the logic of db connection

---
+++ java/book3/webframework02/src/main/java/org/smart4j/chapter2/helper/DatabaseHelper.java
@@ -0,0 +1,57 @@
+package org.smart4j.chapter2.helper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.service.CustomerService;
+import org.smart4j.chapter2.util.PropsUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class DatabaseHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties config = PropsUtil.loadProps("config.properties");
+        DRIVER = config.getProperty("jdbc.driver");
+        URL = config.getProperty("jdbc.url");
+        USERNAME = config.getProperty("jdbc.username");
+        PASSWORD = config.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver class, ", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            LOGGER.error("get connection failure ", e);
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOGGER.error("close connection failure, ", e);
+            }
+        }
+    }
+}
--- java/book3/webframework02/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ java/book3/webframework02/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -8,32 +8,17 @@ import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
 import org.smart4j.chapter2.util.PropsUtil;
+
+import javax.xml.crypto.Data;
 
 /**
  * 提供客户数据服务
  */
 public class CustomerService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
-
-    static {
-        Properties config = PropsUtil.loadProps("config.properties");
-        DRIVER = config.getProperty("jdbc.driver");
-        URL = config.getProperty("jdbc.url");
-        USERNAME = config.getProperty("jdbc.username");
-        PASSWORD = config.getProperty("jdbc.password");
-
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("can not load jdbc driver class, ", e);
-        }
-    }
 
     /**
      * 获取客户列表
@@ -44,7 +29,7 @@ public class CustomerService {
         Connection connection = null;
         try {
             List<Customer> customerList = new ArrayList<>();
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DatabaseHelper.getConnection();
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery();
 
@@ -62,13 +47,7 @@ public class CustomerService {
         } catch (SQLException e) {
             LOGGER.error("execute sql failure, ", e);
         } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    LOGGER.error("close connection failure ", e);
-                }
-            }
+            DatabaseHelper.closeConnection(connection);
         }
         return null;
     }
